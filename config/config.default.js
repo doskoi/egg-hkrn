eadd0edff5eba49e47aca40b64f56ada1c5e2b91
jiaxin <songjiaxin@wallstreetcn.com>
2017-11-27 21:59:18 +0800

    WIP

--- config/config.default.js
+++ config/config.default.js
@@ -6,3 +6,10 @@ exports.view = {
         '.tpl': 'nunjucks',
     },
 };
+
+exports.security = {
+    csrf: {
+      ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
+    },
+ };
+ 
