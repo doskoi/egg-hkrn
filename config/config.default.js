3b9b79d6171a2de7941fa2ad80be884dfb743583
Sheng <doskoi.panda@gmail.com>
2017-11-28 22:01:53 +0800

    add service

--- config/config.default.js
+++ config/config.default.js
@@ -7,9 +7,24 @@ exports.view = {
     },
 };
 
+exports.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
+
+exports.middleware = [
+    'robot'
+  ];
+  // robot's configurations
+  exports.robot = {
+    ua: [
+      /Baiduspider/i,
+    ]
+};
+
 exports.security = {
     csrf: {
       ignoreJSON: true, // 默认为 false，当设置为 true 时，将会放过所有 content-type 为 `application/json` 的请求
     },
- };
+};
  
